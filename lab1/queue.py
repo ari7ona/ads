b1c0bad6f651eec6b307de6c719d7ea8c3da4110
Akhan <a.zhexenbekov@prodengi.kz>
2024-06-03 18:50:24 +0500

    rename methods and add new

--- lab1/queue.py
+++ lab1/queue.py
@@ -1,21 +1,44 @@
 class Queue:
-    def __init__(self):
+
+    def __init__(self, max_size):
         self.container = []
-    def add(self, item):
-        self.container.append(item)
-    def remove(self):
-        if self.container:
+        self.max_size = max_size
+
+    def enqueue(self, item):
+        if not self.isFull():
+            self.container.append(item)
+        else:
+            print("Queue is full")
+
+    def dequeue(self):
+        if not self.isEmpty():
             return self.container.pop(0)
         else:
             print("Queue is empty")
             return None
+        
+    def size(self):
+        return len(self.container)
+    
+    def isEmpty(self):
+        return self.size() == 0
+    
+    def isFull(self):
+        return len(self.container) >= self.max_size
+    
+    def peek(self):
+        if self.isEmpty():
+            raise Exception("Queue is empty")
+        return self.container[-1]
+    
     def show(self):
         return self.container
 
-q = Queue()
-q.add('a')
+q = Queue(5)
+print(q.isFull())
+q.enqueue('a')
 print(q.show())
-q.add('b')
+q.enqueue('b')
 print(q.show())
-q.remove()
+q.dequeue()
 print(q.show())
--- lab1/stack.py
+++ lab1/stack.py
@@ -1,21 +1,39 @@
 class Stack:
+
     def __init__(self):
         self.container = []
-    def add(self, item):
+
+    def push(self, item):
         self.container.append(item)
-    def remove(self):
-        if self.container:
+
+    def pop(self):
+        if not self.isEmpty():
             return self.container.pop()
         else:
             print("Stack is empty")
             return None
+        
+    def isEmpty(self):
+        return self.size() == 0
+    
+    def size(self):
+        return len(self.container)
+    
+    def peek(self):
+        if self.isEmpty():
+            raise Exception("Stack is empty")
+        return self.container[-1]
+    
     def show(self):
         return self.container
 
 s = Stack()
-s.add('a')
+s.push('a')
 print(s.show())
-s.add('b')
+s.push('b')
 print(s.show())
-s.remove()
+s.pop()
 print(s.show())
+print(s.peek())
+print(s.size())
+print(s.isEmpty())
