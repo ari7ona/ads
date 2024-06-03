fff4c4df00468964f9b28a6cfbac0cc1a977e794
Akhan <a.zhexenbekov@prodengi.kz>
2024-06-03 19:57:36 +0500

    deque modifiy

--- lab1/deque.py
+++ lab1/deque.py
@@ -1,22 +1,28 @@
 class Deque:
+
     def __init__(self):
         self.container = []
+
     def add_front(self, item):
         self.container.insert(0, item)
+
     def add_rear(self, item):
         self.container.append(item)
+
     def remove_front(self):
         if self.container:
             return self.container.pop(0)
         else:
             print("Deque is empty")
             return None
+        
     def remove_rear(self):
         if self.container:
             return self.container.pop()
         else:
             print("Deque is empty")
             return None
+        
     def show(self):
         return self.container
 
